c1738e282e39bdb7af1250a5fe592d7fe7ba2d48
ramesh-366 <78030953+ramesh-366@users.noreply.github.com>
2021-02-09 17:34:15 +0530

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
+#include<bits/stdc++.h>
+#define repeat(i,a,b) for(int i=a; i<=b; i++)
+#define _tTimes int t;cin>>t;while(t--)
 using namespace std;
 
 int main(){
-  cout<<"Hello, World";
-  return 0
+    cout<<"Hello, World!"
+    return 0;
 }
